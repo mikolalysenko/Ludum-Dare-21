c21824a4084b3e1fdde3dd23a68a17b4c719e82b
Mikola <mikola@STUPIDHOSTNAME.(none)>
2011-08-21 22:31:56 -0500

    repushing stuff

--- src/entity.cc
+++ src/entity.cc
@@ -34,3 +34,29 @@ void LevelExitEntity::draw() {
 }
 
 
+
+
+TeleporterEntity::~TeleporterEntity() {}
+
+void TeleporterEntity::init() {
+}
+
+void TeleporterEntity::tick(float dt) {
+	auto p = &puzzle->player.particle;
+	float d = (p->coordinate.position - coordinate.position).norm();
+	if(p->coordinate.solid == coordinate.solid && d <= p->radius) {
+		puzzle->level_complete = true;
+	}
+}
+
+void TeleporterEntity::draw() {
+	auto p = coordinate.position;
+	glPointSize(10);
+	glBegin(GL_POINTS);
+	glColor3f(drand48(), drand48(), drand48());
+	glVertex3f(p[0], p[1], p[2]);
+	glEnd();
+}
+
+
+
--- src/entity.h
+++ src/entity.h
@@ -13,5 +13,22 @@ struct LevelExitEntity : public Entity {
 	virtual void draw();
 };
 
+//A teleporter entity
+struct TeleporterEntity : public Entity {
+
+	IntrinsicCoordinate target_coordinate;
+
+	TeleporterEntity(
+		IntrinsicCoordinate const& src,
+		IntrinsicCoordinate const& dst) :
+			Entity(src),
+			target_coordinate(dst) {}
+
+	virtual ~TeleporterEntity();
+	virtual void init();
+	virtual void tick(float dt);
+	virtual void draw();
+};
+
 #endif
 
